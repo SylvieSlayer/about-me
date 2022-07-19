3ec4771e4d3c2add0b48a97525c7ad1704062edc
SYLVIE FISHER <iwalkedwithasylvie@gmail.com>
2022-07-18 20:41:45 -0700

    added code

---
+++ js/app.js
@@ -0,0 +1,19 @@
+'use strict'
+
+let prompt1 = prompt('do you have any pets?');
+
+let prompt2 = prompt ('How many?');
+
+alert ('Awesome!');
+
+let prompt3 = prompt ('What is your favorite book?');
+
+alert ('I will read it!')
+
+let prompt4 = prompt ('Whats Your favorite album');
+
+alert ("I love that one");
+
+let prompt5 = prompt ('what is youre favorite film?');
+
+alert ('mine is texas chain saw massacre');
